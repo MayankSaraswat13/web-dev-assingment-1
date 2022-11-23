2c4b8ed749537114d26f78a1b82181792ecccef9
MayankSaraswat <mayanksaraswat2003@gmail.com>
2022-11-23 12:31:52 +0530

    to do list

--- script.js
+++ script.js
@@ -5,25 +5,29 @@ function addEvent(e) {
     e.preventDefault();
 
     var item=document.getElementById('main-input').value;
-    
-    var newItem=document.createElement('li');
-    newItem.className="list-group-item";
-    newItem.appendChild(document.createTextNode(item));
+    if(item !== ''){
+        var newItem=document.createElement('li');
+        newItem.className="list-group-item";
+        newItem.appendChild(document.createTextNode(item));
 
-    var dltButton=document.createElement('button');
-    dltButton.className="btn btn-danger btn-sm float-right delete";
-    dltButton.appendChild(document.createTextNode('X'));
+        var dltButton=document.createElement('button');
+        dltButton.className="btn btn-danger btn-sm float-right delete";
+        dltButton.appendChild(document.createTextNode('X'));
 
-    newItem.appendChild(dltButton);
+        newItem.appendChild(dltButton);
 
-    itemList.appendChild(newItem);
+        itemList.appendChild(newItem);
+    }
+    else{
+        alert('kuch daal to le');
+    }
 }
 
 itemList.addEventListener('click',removeEvent);
 
 function removeEvent(e) {
     if(e.target.classList.contains('delete')){
-        if(confirm('delete kr rhe ho?')){
+        if(confirm('aren you sure?')){
             var li=e.target.parentElement;
             itemList.removeChild(li);
         }
